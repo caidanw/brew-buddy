88edcde565ef59e1a875b14f16edf300199cb36e
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-12-18 12:49:23 -0700

    fixed header link

--- app/frontend/src/components/layout/Header.js
+++ app/frontend/src/components/layout/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-                <a className="navbar-brand" href="#">Brew Buddy</a>
+                <a className="navbar-brand" href="/">Brew Buddy</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03"
                         aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"/>
